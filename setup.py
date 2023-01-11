340fe6ac0a4c7f6a60f56f511fdf81f101bc1fe5
FDijkema <fmdijkema@gmail.com>
2023-01-11 17:05:44 +0100

    working on getting package built correctly

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
-from setuptools import find_packages
+from setuptools import find_namespace_packages
 
 
 setup(
 name='lumparser',
-packages=find_packages("src"),
+packages=find_namespace_packages(where="src"),
 package_dir={'': 'src'},
 version='0.0.3',
 include_package_data=True,
